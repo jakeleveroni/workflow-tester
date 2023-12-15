d529f3f424ff79d476239387f1f5f01ff0c60d3f
jakeleveroni <jleveroni@figure.com>
2023-12-15 12:56:09 -0800

    feat: move script back into wf cause idk whats wrong with import

--- .github/workflows/scripts/prefix-comment-parser.js
+++
@@ -1,6 +0,0 @@
-module.exports = (context) => {
-    console.log('Jake context', context);
-    const str = context.payload.comment.body;
-    const wsStr = str.replace('/prefix', '').trim().split(',');
-    return wsStr.join(' ');
-}
